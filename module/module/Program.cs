3d7aec0ce66f7ad0487b7a3fab081ad4fd15ae92
Andrey P <petr.and.vic@gmail.com>
2025-02-20 14:51:18 +0400

    Лекция 1.2 Состояние и поведение объектов

--- module/module/Program.cs
+++ module/module/Program.cs
@@ -1,11 +1,23 @@
-﻿// using module;
+﻿using module;
 
 class Program
 {
     public static void Main()
     {
-        A a = new A();
-        Console.Write(a.GetX());
-    }
+        Cat cat1 = new Cat();
+        cat1.Name = "Мурка";
+        cat1.Breed = "Метис";
+        cat1.BirthDay = new DateTime(2020, 6, 29);
+        
+        Cat cat2 = new();
+        cat2.Name = "Барсик";
+        cat2.BirthDay = new DateTime(2019, 9, 19);
+        cat2.Breed = "Дворовая";
 
+        Cat cat3 = cat1;
+
+
+        Console.WriteLine(cat1.Name == cat2.Name);
+        Console.WriteLine(cat1.Name == cat3.Name);
+    }
 }
