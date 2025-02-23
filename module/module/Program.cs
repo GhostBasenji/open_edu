ee8e0f44856685448c0e20df8a926dd8571d85db
Andrey P <petr.and.vic@gmail.com>
2025-02-23 14:33:41 +0400

    Добавил класс Triangle

--- module/module/Program.cs
+++ module/module/Program.cs
@@ -1,48 +1,53 @@
 ﻿using module;
 
-class Point2D
-{
-    double _x;
-    double _y;
+//class Point2D
+//{
+//    double _x;
+//    double _y;
 
-    public void MoveX(double c)
-    {
-        if (IsMovingAccepted(c))
-        {
-            _x += c;
-        }
-    }
+//    public void MoveX(double c)
+//    {
+//        if (IsMovingAccepted(c))
+//        {
+//            _x += c;
+//        }
+//    }
 
-    public bool IsMovingAccepted(double c)
-    {
-        if (c < 0)
-        {
-            return false;
-        }
-        return true;
-    }
-}
+//    public bool IsMovingAccepted(double c)
+//    {
+//        if (c < 0)
+//        {
+//            return false;
+//        }
+//        return true;
+//    }
+// }
 
     class Program
     {
         public static void Main()
         {
-            //Cat cat1 = new Cat();
-            //cat1.Name = "Мурка";
-            //cat1.Breed = "Метис";
-            //cat1.BirthDay = new DateTime(2020, 6, 29);
-
-            //Cat cat2 = new();
-            //cat2.Name = "Барсик";
-            //cat2.BirthDay = new DateTime(2019, 9, 19);
-            //cat2.Breed = "Дворовая";
-
-            //Cat cat3 = cat1;
+        Triangle t = new();
+        t.B = 12;
+        Console.WriteLine($"Hypotenuse: {t.C}");
 
 
-            //Console.WriteLine(cat1.Name == cat2.Name);
-            //Console.WriteLine(cat1.Name == cat3.Name);
-        }
+        //Cat cat1 = new Cat();
+        //cat1.Name = "Мурка";
+        //cat1.Breed = "Метис";
+        //cat1.BirthDay = new DateTime(2020, 6, 29);
 
+        //Cat cat2 = new();
+        //cat2.Name = "Барсик";
+        //cat2.BirthDay = new DateTime(2019, 9, 19);
+        //cat2.Breed = "Дворовая";
+
+        //Cat cat3 = cat1;
+
+
+        //Console.WriteLine(cat1.Name == cat2.Name);
+        //Console.WriteLine(cat1.Name == cat3.Name);
     }
+
+}
     
---
+++ module/module/Triangle.cs
@@ -0,0 +1,42 @@
+﻿namespace module
+{
+    internal class Triangle
+    {
+        double _a = 3;
+        double _b = 4;
+
+        public double A
+        {
+            get { return _a; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _a = value;
+                }
+            }
+        }
+
+        public double B
+        {
+            get { return _b; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _b = value;
+                }
+            }
+        }
+
+        public double C
+        {
+            get
+            {
+                return Math.Sqrt((_a * _a) + (_b * _b));
+            }
+        }
+    }
+}
