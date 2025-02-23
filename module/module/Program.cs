921cd3f3e72861dcea996efbffa8c71f5d7fa6ac
Andrey P <petr.and.vic@gmail.com>
2025-02-23 14:17:19 +0400

    Добавил класс Point2D для примера

--- module/module/Program.cs
+++ module/module/Program.cs
@@ -1,23 +1,48 @@
 ﻿using module;
 
-class Program
+class Point2D
 {
-    public static void Main()
+    double _x;
+    double _y;
+
+    public void MoveX(double c)
     {
-        Cat cat1 = new Cat();
-        cat1.Name = "Мурка";
-        cat1.Breed = "Метис";
-        cat1.BirthDay = new DateTime(2020, 6, 29);
-        
-        Cat cat2 = new();
-        cat2.Name = "Барсик";
-        cat2.BirthDay = new DateTime(2019, 9, 19);
-        cat2.Breed = "Дворовая";
+        if (IsMovingAccepted(c))
+        {
+            _x += c;
+        }
+    }
 
-        Cat cat3 = cat1;
-
-
-        Console.WriteLine(cat1.Name == cat2.Name);
-        Console.WriteLine(cat1.Name == cat3.Name);
+    public bool IsMovingAccepted(double c)
+    {
+        if (c < 0)
+        {
+            return false;
+        }
+        return true;
     }
 }
+
+    class Program
+    {
+        public static void Main()
+        {
+            //Cat cat1 = new Cat();
+            //cat1.Name = "Мурка";
+            //cat1.Breed = "Метис";
+            //cat1.BirthDay = new DateTime(2020, 6, 29);
+
+            //Cat cat2 = new();
+            //cat2.Name = "Барсик";
+            //cat2.BirthDay = new DateTime(2019, 9, 19);
+            //cat2.Breed = "Дворовая";
+
+            //Cat cat3 = cat1;
+
+
+            //Console.WriteLine(cat1.Name == cat2.Name);
+            //Console.WriteLine(cat1.Name == cat3.Name);
+        }
+
+    }
+    
